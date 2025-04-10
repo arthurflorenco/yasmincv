c305f1ef828a4e3ec19622046e46e635134a3a86
Arthur Florenço <135295547+arthurflorenco@users.noreply.github.com>
2025-04-10 10:32:48 -0300

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
-import { withContentlayer } from "next-contentlayer";
+import { withContentlayer } from 'next-contentlayer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'], // Suporte para arquivos MDX
+  experimental: {
+    mdxRs: true, // Habilita o uso do novo parser para MDX
+  },
+  webpack(config, { dev, isServer }) {
+    // Adiciona configuração para processar CSS
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // Usando style-loader e css-loader para CSS
+      });
+    }
+
+    return config;
+  },
 };
 
 export default withContentlayer(nextConfig);
